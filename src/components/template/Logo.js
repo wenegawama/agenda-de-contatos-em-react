a43115878512ef644ddeb8b741dcb755321edfb2
Wenega Wama <narcissewatherloy@gmail.com>
2021-07-15 22:45:35 -0300

    styled logo

--- src/components/template/Logo.js
+++ src/components/template/Logo.js
@@ -1,7 +1,11 @@
-import React from 'react'
 import './Logo.css'
+import logo from '../../assets/images/logo-contact.png'
+import React from 'react'
+
 
 export default props => 
     <aside className="logo">
-        Logo
+        <a href="/" className="logo">
+            <img src={logo} alt="logo"/>
+        </a>
     </aside>
